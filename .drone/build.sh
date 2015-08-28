516693177cb230652ed37126b7c385eb219f2f84
calvinpy <dani@aplazame.com>
2015-08-28 18:26:48 +0200

    fix build.sh

--- .drone/build.sh
+++ .drone/build.sh
@@ -8,6 +8,7 @@ set -e
 case $DRONE_BRANCH in
     master)
         echo master, all checks have passed
+        ;;
     *)
         echo *$DRONE_BRANCH* pull request, all checks have passed
         ;;
