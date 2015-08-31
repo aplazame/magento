bd4d335bba689f56fd5f392930de1d38ce8ee045
calvinpy <dani@aplazame.com>
2015-08-31 15:22:32 +0200

    deployment with zsh

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,2 +1,2 @@
 # Deploy to demo
-ssh -i ~/.ssh/id_rsa.prod $DEPLOY_USER@aplazame.com "bash -c 'source ~/.bashrc;cd $MAGENTO_PATH;modgit update Magento_Aplazame'"
+ssh -i ~/.ssh/id_rsa.prod $DEPLOY_USER@aplazame.com "zsh -c 'source ~/.zshrc;cd $MAGENTO_PATH;modgit update Magento_Aplazame'"
