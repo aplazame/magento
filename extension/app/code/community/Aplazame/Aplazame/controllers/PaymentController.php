5d90b18ded03c5fb4a8a45a5117b20b1f6a37d22
calvinpy <dani@aplazame.com>
2015-10-06 17:40:28 +0200

    fix redirect to Url

--- extension/app/code/community/Aplazame/Aplazame/controllers/PaymentController.php
+++ extension/app/code/community/Aplazame/Aplazame/controllers/PaymentController.php
@@ -60,7 +60,7 @@ class Aplazame_Aplazame_PaymentController extends Mage_Core_Controller_Front_Act
             // $this->_redirect('checkout/onepage/success');
             return;
         }
-        // $this->_redirect(Mage::helper('checkout/url')->getCheckoutUrl());
+        // $this->_redirectUrl(Mage::helper('checkout/url')->getCheckoutUrl());
     }
 
     public function cancelAction()
@@ -75,7 +75,7 @@ class Aplazame_Aplazame_PaymentController extends Mage_Core_Controller_Front_Act
             }
         }
 
-        $this->_redirect(Mage::helper('checkout/url')->getCheckoutUrl());
+        $this->_redirectUrl(Mage::helper('checkout/url')->getCheckoutUrl());
     }
 
     public function historyAction()
