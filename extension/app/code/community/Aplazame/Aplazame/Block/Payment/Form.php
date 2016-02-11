4438e7a44acacdf791c045f07a769b86cd7a9977
Oscar Reales <oreales@gmail.com>
2016-02-11 15:50:35 +0100

    Revert "v0.2.2 permitiendo cambiar nombre del metodo en el checkout"
    This reverts commit 2f1ed24d734de5d5169fe915a2dce68f9a1edbb5.

--- extension/app/code/community/Aplazame/Aplazame/Block/Payment/Form.php
+++ extension/app/code/community/Aplazame/Aplazame/Block/Payment/Form.php
@@ -14,20 +14,13 @@ class Aplazame_Aplazame_Block_Payment_Form extends Mage_Payment_Block_Form
 
     private function setMethodLabel()
     {
-        $_title = Mage::getStoreConfig('payment/aplazame/title');
-        $this->setMethodTitle($_title);
+        $this->setMethodTitle("");
 
-        if(!$_title)
-        {
-            $logoSrc = 'https://aplazame.com/static/img/buttons/' . Mage::getStoreConfig('payment/aplazame/button_img') . '.png';
-            $html = '<img src="' . $logoSrc . '" height="27" class="v-middle" />&nbsp;';
-            $html.= 'Financia tu compa con Aplazame';
+        $logoSrc = 'https://aplazame.com/static/img/buttons/' . Mage::getStoreConfig('payment/aplazame/button_img') . '.png';
+        $html = '<img src="' . $logoSrc . '" height="27" class="v-middle" />&nbsp;';
+        $html.= 'Financia tu compa con Aplazame';
 
-            $this->setMethodLabelAfterHtml($html);
-        }
-
-        return $this;
-
+        $this->setMethodLabelAfterHtml($html);
     }
 
     public function getTotal()
