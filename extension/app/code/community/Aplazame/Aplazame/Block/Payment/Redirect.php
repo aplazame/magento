9ba7b34f6784cc5368589ae73fdad3d5c6c67764
Maks3w <github.maks3w@virtualplanets.net>
2016-07-05 21:32:48 +0200

    Remove Host setting
    environment variables provide similar behavior
    
    APLAZAME_API_BASE_URI
    APLAZAME_JS_URI

--- extension/app/code/community/Aplazame/Aplazame/Block/Payment/Redirect.php
+++ extension/app/code/community/Aplazame/Aplazame/Block/Payment/Redirect.php
@@ -5,6 +5,8 @@ class Aplazame_Aplazame_Block_Payment_Redirect extends Mage_Core_Block_Abstract
 {
     protected function _toHtml()
     {
+        $aplazameJsUri = getenv('APLAZAME_JS_URI') ? getenv('APLAZAME_JS_URI') : 'https://aplazame.com/static/aplazame.js';
+
         /** @var Aplazame_Aplazame_Model_Payment $payment */
         $payment = Mage::getModel('aplazame/payment');
 
@@ -14,7 +16,7 @@ class Aplazame_Aplazame_Block_Payment_Redirect extends Mage_Core_Block_Abstract
 
         <script
             type="text/javascript"
-            src="'. trim(Mage::getStoreConfig('payment/aplazame/host'), "/") . '/static/aplazame.js' . '"
+            src="'. $aplazameJsUri . '"
             data-aplazame="publicKey: '. Mage::getStoreConfig('payment/aplazame/public_api_key') . '"
             data-sandbox="'. (Mage::getStoreConfig('payment/aplazame/sandbox')?'true':'false') . '">
         </script>
--- extension/app/code/community/Aplazame/Aplazame/Model/Api/Client.php
+++ extension/app/code/community/Aplazame/Aplazame/Model/Api/Client.php
@@ -8,18 +8,19 @@ class Aplazame_Aplazame_Model_Api_Client extends Varien_Object
 {
     const API_CHECKOUT_PATH = '/orders';
 
+    /**
+     * @var string
+     */
+    private $apiBaseUri;
+
     public function __construct()
     {
-    }
-
-    public function getBaseApiUrl()
-    {
-        return str_replace('://', '://api.', Mage::getStoreConfig('payment/aplazame/host'));
+        $this->apiBaseUri = getenv('APLAZAME_API_BASE_URI') ? getenv('APLAZAME_API_BASE_URI') : 'https://api.aplazame.com';
     }
 
     protected function _api_request($method, $path, $data=null)
     {
-        $url = trim($this->getBaseApiUrl(), "/") . self::API_CHECKOUT_PATH . $path;
+        $url = $this->apiBaseUri. self::API_CHECKOUT_PATH . $path;
         $client = new Zend_Http_Client($url);
 
         if (in_array($method, array(
