bf2604dabf63d725ccf0480cde75ba4a344efafb
Jesús Germade <jesus@germade.es>
2016-08-01 23:01:20 +0200

    setting api-host in checkout

--- extension/app/code/community/Aplazame/Aplazame/Block/Payment/Redirect.php
+++ extension/app/code/community/Aplazame/Aplazame/Block/Payment/Redirect.php
@@ -7,6 +7,9 @@ class Aplazame_Aplazame_Block_Payment_Redirect extends Mage_Core_Block_Abstract
     {
         $aplazameJsUri = getenv('APLAZAME_JS_URI') ? getenv('APLAZAME_JS_URI') : 'https://aplazame.com/static/aplazame.js';
 
+        /** @var Aplazame_Aplazame_Model_Api_Client $client */
+        $client = Mage::getModel('aplazame/api_client');
+
         /** @var Aplazame_Aplazame_Model_Payment $payment */
         $payment = Mage::getModel('aplazame/payment');
 
@@ -17,7 +20,8 @@ class Aplazame_Aplazame_Block_Payment_Redirect extends Mage_Core_Block_Abstract
         <script
             type="text/javascript"
             src="'. $aplazameJsUri . '"
-            data-aplazame="publicKey: '. Mage::getStoreConfig('payment/aplazame/public_api_key') . '"
+            data-api-host="' . $client->apiBaseUri . '"
+            data-aplazame="'. Mage::getStoreConfig('payment/aplazame/public_api_key') . '"
             data-sandbox="'. (Mage::getStoreConfig('payment/aplazame/sandbox')?'true':'false') . '">
         </script>
 
