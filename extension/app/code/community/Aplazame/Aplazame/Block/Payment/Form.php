76e7ad1e32ba770c90903a33a285c2bc72d7d1d1
Maks3w <Maks3w@users.noreply.github.com>
2016-11-24 12:58:35 +0100

    Typo
    Kudos @alexsancho

--- extension/app/code/community/Aplazame/Aplazame/Block/Payment/Form.php
+++ extension/app/code/community/Aplazame/Aplazame/Block/Payment/Form.php
@@ -15,7 +15,7 @@ class Aplazame_Aplazame_Block_Payment_Form extends Mage_Payment_Block_Form
 
         $logoSrc = 'https://aplazame.com/static/img/buttons/' . Mage::getStoreConfig('payment/aplazame/button_img') . '.png';
         $html = '<img src="' . $logoSrc . '" height="27" class="v-middle" />&nbsp;';
-        $html.= 'Financia tu compa con Aplazame';
+        $html.= 'Financia tu compra con Aplazame';
 
         $this->setMethodLabelAfterHtml($html);
     }
