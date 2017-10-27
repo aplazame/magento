98462a4efc992310dc1e95b4e3ef511428a6f3e8
Maks3w <github.maks3w@virtualplanets.net>
2017-10-27 20:31:36 +0200

    Async confirm

--- extension/app/code/community/Aplazame/Aplazame/controllers/ApiController.php
+++ extension/app/code/community/Aplazame/Aplazame/controllers/ApiController.php
@@ -24,6 +24,18 @@ class Aplazame_Aplazame_ApiController extends Mage_Core_Controller_Front_Action
         );
     }
 
+    public static function client_error($detail)
+    {
+        return array(
+            'status_code' => 400,
+            'payload' => array(
+                'status' => 400,
+                'type' => 'CLIENT_ERROR',
+                'detail' => $detail,
+            ),
+        );
+    }
+
     public static function collection($page, $page_size, array $elements)
     {
         return array(
@@ -44,8 +56,9 @@ class Aplazame_Aplazame_ApiController extends Mage_Core_Controller_Front_Action
         $path = $request->getParam('path', '');
         $pathArguments = json_decode($request->getParam('path_arguments', '[]'), true);
         $queryArguments = json_decode($request->getParam('query_arguments', '[]'), true);
+        $payload = json_decode($request->getRawBody(), true);
 
-        $result = $this->route($path, $pathArguments, $queryArguments);
+        $result = $this->route($path, $pathArguments, $queryArguments, $payload);
 
         $response = $this->getResponse();
         $response->setHttpResponseCode($result['status_code']);
@@ -57,10 +70,11 @@ class Aplazame_Aplazame_ApiController extends Mage_Core_Controller_Front_Action
      * @param string $path
      * @param array $pathArguments
      * @param array $queryArguments
+     * @param null|array $payload
      *
      * @return array
      */
-    public function route($path, array $pathArguments, array $queryArguments)
+    public function route($path, array $pathArguments, array $queryArguments, $payload)
     {
         if (!$this->verifyAuthentication()) {
             return self::forbidden();
@@ -71,6 +85,13 @@ class Aplazame_Aplazame_ApiController extends Mage_Core_Controller_Front_Action
                 $controller = new Aplazame_Aplazame_Api_Article(Mage::getModel('catalog/product'));
 
                 return $controller->articles($queryArguments);
+            case '/confirm/':
+                $controller = new Aplazame_Aplazame_Api_Confirm(
+                    Mage::getModel('sales/order'),
+                    Mage::getStoreConfig('payment/aplazame/sandbox')
+                );
+
+                return $controller->confirm($payload);
             case '/order/{order_id}/history/':
                 $controller = new Aplazame_Aplazame_Api_Order(Mage::getModel('sales/order'));
 
