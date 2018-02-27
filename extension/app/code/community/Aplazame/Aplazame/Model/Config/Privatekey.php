da9241d76625f9dfe07226c31db48398fe8e0964
Maks3w <github.maks3w@virtualplanets.net>
2018-02-27 09:56:03 +0100

    CS / MEQP1 rules

--- extension/app/code/community/Aplazame/Aplazame/Model/Config/Privatekey.php
+++ extension/app/code/community/Aplazame/Aplazame/Model/Config/Privatekey.php
@@ -10,15 +10,20 @@ class Aplazame_Aplazame_Model_Config_Privatekey extends Mage_Adminhtml_Model_Sys
             $privateKey
         );
 
-        $response = $client->patch('/me', array(
-            'confirmation_url' => Mage::getUrl('aplazame/api/index', array(
-                '_query' => array(
-                    'path' => '/confirm/',
+        $response = $client->patch(
+            '/me',
+            array(
+                'confirmation_url' => Mage::getUrl(
+                    'aplazame/api/index', array(
+                        '_query' => array(
+                            'path' => '/confirm/',
+                        ),
+                        '_nosid' => true,
+                        '_store' => Mage::app()->getDefaultStoreView(),
+                    )
                 ),
-                '_nosid' => true,
-                '_store' => Mage::app()->getDefaultStoreView(),
-            )),
-        ));
+            )
+        );
 
         return $response;
     }
