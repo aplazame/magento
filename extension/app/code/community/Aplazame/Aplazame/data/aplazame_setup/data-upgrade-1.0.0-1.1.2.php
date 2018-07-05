70a7aa1da990d2fe0f26481811f2761091e451d9
Maks3w <github.maks3w@virtualplanets.net>
2018-07-05 17:00:11 +0200

    [notification_url] Don't set confirmation_url

--- extension/app/code/community/Aplazame/Aplazame/data/aplazame_setup/data-upgrade-1.0.0-1.1.2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$privateKey = Mage::getStoreConfig('payment/aplazame/secret_api_key');
-if (!empty($privateKey)) {
-    /** @var Mage_Core_Model_Config_Data $configData */
-    $configData = Mage::getModel('core/config_data');
-
-    Aplazame_Aplazame_Model_Config_Privatekey::setAplazameMerchantParams($privateKey);
-}
