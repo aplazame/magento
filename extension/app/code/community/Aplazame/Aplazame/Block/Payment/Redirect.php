bb5520c2bd29b6facf314d56fe8e377a409810eb
Alejandro Hernández <alejandro.herord@gmail.com>
2019-04-11 11:06:29 +0200

    Async load from aplazame.js

--- extension/app/code/community/Aplazame/Aplazame/Block/Payment/Redirect.php
+++ extension/app/code/community/Aplazame/Aplazame/Block/Payment/Redirect.php
@@ -4,7 +4,8 @@ class Aplazame_Aplazame_Block_Payment_Redirect extends Mage_Core_Block_Abstract
 {
     protected function _toHtml()
     {
-        $aplazameJsUri = getenv('APLAZAME_JS_URI') ? getenv('APLAZAME_JS_URI') : 'https://cdn.aplazame.com/aplazame.js';
+        $aplazameJsUri = getenv('APLAZAME_JS_URI') ? getenv('APLAZAME_JS_URI') : 'https://cdn.aplazame.com/aplazame.js?public_key='
+            . Mage::getStoreConfig('payment/aplazame/public_api_key') . '&sandbox=' . (Mage::getStoreConfig('payment/aplazame/sandbox') ? 'true' : 'false');
 
         /** @var Aplazame_Aplazame_Model_Api_Client $client */
         $client = Mage::getModel('aplazame/api_client');
@@ -18,16 +19,12 @@ class Aplazame_Aplazame_Block_Payment_Redirect extends Mage_Core_Block_Abstract
 <html>
     <body style="margin: 0;">
 
-        <script
-            type="text/javascript"
-            src="' . $aplazameJsUri . '"
-            data-api-host="' . $client->apiBaseUri . '"
-            data-aplazame="' . Mage::getStoreConfig('payment/aplazame/public_api_key') . '"
-            data-sandbox="' . (Mage::getStoreConfig('payment/aplazame/sandbox') ? 'true' : 'false') . '">
-        </script>
+        <script type="text/javascript" src="' . $aplazameJsUri . '" data-api-host="' . $client->apiBaseUri . '" async defer></script>
 
         <script>
-            aplazame.checkout("' . $payload['id'] . '");
+            (window.aplazame = window.aplazame || []).push(function (aplazame) {
+                aplazame.checkout("' . $payload['id'] . '")
+            })
         </script>
 
         <iframe src="' . /*Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB)*/Mage::getUrl('', array('_secure' => true)) . '" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden;">
