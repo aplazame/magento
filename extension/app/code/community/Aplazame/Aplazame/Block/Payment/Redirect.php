5988e79361eb2da746e09db043490a4109278406
Alejandro Hernández <alejandro.herord@gmail.com>
2019-05-28 15:21:59 +0200

    Delete 'host' as query param

--- extension/app/code/community/Aplazame/Aplazame/Block/Payment/Redirect.php
+++ extension/app/code/community/Aplazame/Aplazame/Block/Payment/Redirect.php
@@ -4,16 +4,12 @@ class Aplazame_Aplazame_Block_Payment_Redirect extends Mage_Core_Block_Abstract
 {
     protected function _toHtml()
     {
-        /** @var Aplazame_Aplazame_Model_Api_Client $client */
-        $client = Mage::getModel('aplazame/api_client');
-
         $aplazameJsUri = getenv('APLAZAME_JS_URI') ? getenv('APLAZAME_JS_URI') : 'https://cdn.aplazame.com/aplazame.js';
 
         $aplazameJsParams = http_build_query(
             array(
                 'public_key' => Mage::getStoreConfig('payment/aplazame/public_api_key'),
                 'sandbox' => Mage::getStoreConfig('payment/aplazame/sandbox') ? 'true' : 'false',
-                'host' => $client->apiBaseUri,
             )
         );
 
